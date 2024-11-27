15ff2b1c5943c367afcfcfad0290a7cad10956fa
intesmern2401 <intesmern2401@gmail.com>
2024-11-27 15:46:54 +0600

    navber banenr and achivemnt done

--- customCss/tailwindConfig.js
+++ customCss/tailwindConfig.js
@@ -2,8 +2,15 @@ tailwind.config = {
     theme: {
       extend: {
         colors: {
-          sajib: '#000',
-        }
+          "primary":"#CF0000",
+          "secondary": '#342B27',
+          "third": '#1F1E1E',
+        },
+        maxWidth: {
+          'container': '1320px',
+        },fontFamily: {
+          'inter': ["Inter", "sans-serif"],
+        },
       }
     }
   }
