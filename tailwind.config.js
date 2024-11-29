01f013c290872ff39c0b03782d8e121d9553a219
sajibkhandev <sajibkhan.mern@gmail.com>
2024-11-29 17:49:21 +0600

    change project tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        "primary":"#CF0000",
+        "secondary": '#342B27',
+        "third": '#1F1E1E',
+      },
+      maxWidth: {
+        'container': '1320px',
+      },
+      fontFamily: {
+        'inter': ["Inter", "sans-serif"],
+        'trirong': ["Trirong", "serif"],
+        'hind': ["Hind", "sans-serif"],
+      },
+      backgroundImage: {
+        "sajib1":"url(./images/serviceImage.png)"
+      }
+    }
+  },
+  plugins: [],
+}
