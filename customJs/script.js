af8d15b584380ef79aeb3ba2b6be16ecd44d95b8
sajibkhandev <sajibkhan.mern@gmail.com>
2024-12-06 22:18:39 +0600

    responsive adding

--- customJs/script.js
+++ customJs/script.js
@@ -4,13 +4,26 @@ const workingBtn = document.getElementById("working-btn");
 const membersBtn = document.getElementById("members-btn");
 const concernsBtn = document.getElementById("concerns-btn");
 
-coursesBtn.addEventListener("click", () => {
+const projectPart = document.getElementById("project-part");
+const coursePart = document.getElementById("course-part");
 
-    projectsBtn.classList.add("bg-white", "text-red-500");
+const workingPart = document.getElementById("working-part");
+const memberPart = document.getElementById("member-part");
+const concrenPart = document.getElementById("concren-part");
+
+
+
+// Our Service Function Start
+coursesBtn.addEventListener("click", () => {
+  projectsBtn.classList.add("bg-white", "text-red-500");
   coursesBtn.classList.add("bg-red-500", "text-white");
 
   projectsBtn.classList.remove("bg-red-500", "text-white");
   coursesBtn.classList.remove("bg-white", "text-red-500");
+
+  projectPart.classList.add("hidden")
+  coursePart.classList.remove("hidden")
+  
  
 });
 
@@ -20,8 +33,15 @@ projectsBtn.addEventListener("click", () => {
   
     coursesBtn.classList.remove("bg-red-500", "text-white");
     projectsBtn.classList.remove("bg-white", "text-red-500");
+
+    projectPart.classList.remove("hidden")
+    coursePart.classList.add("hidden")
   
 });
+// Our Service Function end
+
+
+
 
 function toggleActiveButton(activeButton) {
     
@@ -39,3 +59,23 @@ function toggleActiveButton(activeButton) {
   workingBtn.addEventListener("click", () => toggleActiveButton(workingBtn));
   membersBtn.addEventListener("click", () => toggleActiveButton(membersBtn));
   concernsBtn.addEventListener("click", () => toggleActiveButton(concernsBtn));
+
+
+
+// Responsive Button Start
+const menuButton = document.getElementById('menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
+// Responsive Button Start
+
+
+// Responsive Button Start
+
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+  menuIcon.classList.toggle('hidden');
+  closeIcon.classList.toggle('hidden');
+  
+});
+// Responsive Button Start
