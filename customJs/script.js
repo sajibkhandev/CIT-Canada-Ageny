e7ad32ffa0669c04a4df779fbd489224550ac33b
sajibkhandev <sajibkhan.mern@gmail.com>
2024-12-08 17:01:59 +0600

    responsive try to fixed design

--- customJs/script.js
+++ customJs/script.js
@@ -8,7 +8,7 @@ const projectPart = document.getElementById("project-part");
 const coursePart = document.getElementById("course-part");
 
 const workingPart = document.getElementById("working-part");
-const memberPart = document.getElementById("member-part");
+const memberPart = document.getElementById("muember-part");
 const concrenPart = document.getElementById("concren-part");
 
 
@@ -23,8 +23,6 @@ coursesBtn.addEventListener("click", () => {
 
   projectPart.classList.add("hidden")
   coursePart.classList.remove("hidden")
-  
- 
 });
 
 projectsBtn.addEventListener("click", () => {
@@ -36,32 +34,60 @@ projectsBtn.addEventListener("click", () => {
 
     projectPart.classList.remove("hidden")
     coursePart.classList.add("hidden")
-  
 });
 // Our Service Function end
 
 
-
-
+// Companies Function Start
 function toggleActiveButton(activeButton) {
+
+  // Button Design Chanage Start
     
     [workingBtn, membersBtn, concernsBtn].forEach((btn) => {
       btn.classList.remove("bg-red-500", "text-white");
       btn.classList.add("bg-white", "text-red-500");
+      
     });
-  
-    
     activeButton.classList.add("bg-red-500", "text-white");
     activeButton.classList.remove("bg-white", "text-red-500");
+
+  // Button Design Chanage Start
+
+  // Layout Design Chanage Start
+
+
+    [workingPart,memberPart,concrenPart].forEach((btn2)=>{
+      btn2.classList.add("hidden")
+    })
+   
+    if(activeButton==workingBtn){
+      workingPart.classList.remove("hidden")
+      workingPart.classList.add("block")
+     
+    }else if(activeButton==membersBtn){
+      memberPart.classList.remove("hidden")
+      memberPart.classList.add("block")
+     
+      
+    }else if(activeButton==concernsBtn){
+      concrenPart.classList.remove("hidden")
+      concrenPart.classList.add("block")
+    }
+    // Layout Design Chanage Start
+
+
   }
   
- 
   workingBtn.addEventListener("click", () => toggleActiveButton(workingBtn));
   membersBtn.addEventListener("click", () => toggleActiveButton(membersBtn));
   concernsBtn.addEventListener("click", () => toggleActiveButton(concernsBtn));
 
+// Companies Function End
 
 
+
+
+  
 // Responsive Button Start
 const menuButton = document.getElementById('menu-button');
 const mobileMenu = document.getElementById('mobile-menu');
@@ -69,13 +95,10 @@ const menuIcon = document.getElementById('menu-icon');
 const closeIcon = document.getElementById('close-icon');
 // Responsive Button Start
 
-
 // Responsive Button Start
-
 menuButton.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
   menuIcon.classList.toggle('hidden');
   closeIcon.classList.toggle('hidden');
-  
 });
 // Responsive Button Start
